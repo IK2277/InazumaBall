30bdca91461dbbd9460d3109c6b24b39e5a6d743
IK2277 <k.iki22.baseball@gmail.com>
2024-09-23 18:51:11 +0900

    kubota追加

---
+++ InazumaBlitzBall/Assets/Script/kubota.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kubota : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //kubota‚Å‚·
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
