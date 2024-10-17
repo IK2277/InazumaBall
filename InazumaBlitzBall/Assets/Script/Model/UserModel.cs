97d3cc8a3b4b2918ec1758e6ff4fb8ec5507b648
IK2277 <k.iki22.baseball@gmail.com>
2024-10-17 12:20:45 +0900

    ver1.1 ステータス概念を追加

--- InazumaBlitzBall/Assets/Script/Model/UserModel.cs
+++ InazumaBlitzBall/Assets/Script/Model/UserModel.cs
@@ -13,6 +13,7 @@ public class UserModel : MonoBehaviour
     [SerializeField] Game game; //Gameスクリプト]
     public bool isKeeper = false; //キーパー判定
     public bool isUser = true; //操作中判定
+    public bool isBallCatch = false;//ボール所持判定.
     public float moveSpeed = 5.0f; //移動速度
 
     //private変数
@@ -25,6 +26,37 @@ public class UserModel : MonoBehaviour
     float smoothTime = 0.1f; //進行方向にかかるおおよその時間
     float maxAngularVelocity = Mathf.Infinity; //最大の回転角速度
     float angle; //回転角度
+
+    // キャラクターデータ(初期ステータス)
+    [Header("キャラクター名")]
+    public string charaName; // キャラクター名
+    [Header("最大HP(初期HP)")]
+    public int maxHP; // 最大HP→ボール持ってると減少、アビリティ使用で減少、なくなるとコマンドバトルは基本敗北.
+    [Header("スピード")]
+    public int spd; // スピード→移動の速さ.
+    [Header("パス")]
+    public int pas; // パス→能力が高いほどカットされにくくなる.
+    [Header("シュート")]
+    public int sht; // シュート → 能力が高いほどシュートの威力が上がる.
+    [Header("耐久力")]
+    public int phy; // 耐久力 → ドリブルの際、相手のアタックに耐えられるか.
+    [Header("パス・シュートカット")]
+    public int cut; // カット→パスorシュートを途中でカットする力.
+    [Header("アタック")]
+    public int atk; // アタック→ドリブルを止める力.
+    [Header("キャッチ")]
+    public int cat; // キャッチ→シュートを止める力.ランダムで70%-150%で変動.
+    [Header("属性")]
+    public Attribute attribute; // 属性
+    [HideInInspector]
+	public int nowHP; // 現在HP
+    public enum Attribute
+    {
+        Water, // 水属性
+        Fire,  // 火属性
+        Wind,  // 風属性
+        Soil,  // 土属性
+    }
 
     void Start()
     {
